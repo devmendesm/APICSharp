07d8ba8170cbfa7aef8003a04f27b14e9079e02d
devmendesm <matheusviniciusmendes@gmail.com>
2026-07-30 19:07:23 -0300

    FEAT:acesso ao modelo de configuração por injeção de dependencia na categoriasController

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -30,7 +30,7 @@ namespace APICatalogo.Controllers
         }
 
         [HttpGet("{id:int:min(1)}", Name="ObterProduto")]
-        public async Task<ActionResult<Produto>> GetAsync(int id)
+        public async Task<ActionResult<Produto>> GetAsync([FromQuery]int id)
         {
             var produto = await _context.Produtos.FirstOrDefaultAsync(p => p.ProdutoId == id);
             if (produto is null)
